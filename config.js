d3210914cfc85a681622386bedc4990606c8a74b
noritake32 <n-takeda@hubest.jp>
2026-08-29 10:31:29 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // GitHub Pages側の設定
 // 例: https://script.google.com/macros/s/AKfycbxP2s670nyDqhrsyfHd63FbrDYT_ZRPND5e6ZsEO5cHKtqe0wd2gyAqPEnlZVLX-m8G/exec
 window.KINTAI_CONFIG = {
-  gasWebAppUrl: 'https://script.google.com/macros/s/AKfycbxP2s670nyDqhrsyfHd63FbrDYT_ZRPND5e6ZsEO5cHKtqe0wd2gyAqPEnlZVLX-m8G/exec'
+  gasWebAppUrl: 'https://script.google.com/macros/s/AKfycbzlHgfl1ggoWCp3vXX3gtKf7aQaGAGpWycUPo1pbonWou12Ul-NuQtAtuRyBB5n79z8/exec'
 };
